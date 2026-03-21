5ce350ac7a0bd8f1bd9d2e8d55ecf8a90724f721
Kevin-nav <nchorkevin@gmail.com>
2026-03-21 12:01:56 +0000

    feat: register question bank metadata

--- src/database.py
+++ src/database.py
@@ -1,11 +1,24 @@
 from src.infra.db.base import Base
-from src.infra.db.models import AnalyticsEvent, TelegramIdentity, User, WebhookEvent
+from src.infra.db.models import (
+    AnalyticsEvent,
+    QuestionAssetVariant,
+    QuestionAttempt,
+    QuestionBank,
+    StudentCourseState,
+    TelegramIdentity,
+    User,
+    WebhookEvent,
+)
 from src.infra.db.session import AsyncSessionLocal, engine, get_db
 
 __all__ = [
     "AnalyticsEvent",
     "AsyncSessionLocal",
     "Base",
+    "QuestionAssetVariant",
+    "QuestionAttempt",
+    "QuestionBank",
+    "StudentCourseState",
     "TelegramIdentity",
     "User",
     "WebhookEvent",
